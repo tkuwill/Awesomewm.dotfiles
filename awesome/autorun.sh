94a08b9e8ca77d2064c2a7b20a9c9dc0db555dae
tkuwill <billwang1023@gmail.com>
2022-03-31 10:45:06 +0800

    Update configs

---
+++ awesome/autorun.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+
+run xfce4-power-manager
+run systemsettings
+run thunderbird
+run copyq
---
+++ awesome/pastel.lua
@@ -0,0 +1,58 @@
+--      ██████╗  █████╗ ███████╗████████╗███████╗██╗
+--      ██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║
+--      ██████╔╝███████║███████╗   ██║   █████╗  ██║
+--      ██╔═══╝ ██╔══██║╚════██║   ██║   ██╔══╝  ██║
+--      ██║     ██║  ██║███████║   ██║   ███████╗███████╗
+--      ╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚══════╝
+
+-- ===================================================================
+-- Initialization
+-- ===================================================================
+
+
+local awful = require("awful")
+local gears = require("gears")
+
+local pastel = {}
+
+
+-- ===================================================================
+-- Pastel setup
+-- ===================================================================
+
+
+pastel.initialize = function()
+   -- Import components
+   require("components.pastel.wallpaper")
+   require("components.exit-screen")
+   require("components.volume-adjust")
+   require("components.volume-adjust2")
+   require("awesome-wm-widgets.mpris-widget")
+   -- Import panels
+   local left_panel = require("components.pastel.left-panel")
+   local top_panel = require("components.pastel.top-panel")
+
+   -- Set up each screen (add tags & panels)
+   awful.screen.connect_for_each_screen(function(s)
+      for i = 1, 9, 1
+      do
+         awful.tag.add(i, {
+            icon = gears.filesystem.get_configuration_dir() .. "/icons/tags/pastel/" .. i .. ".png",
+            icon_only = true,
+            layout = awful.layout.suit.tile,
+            screen = s,
+            selected = i == 1
+         })
+      end
+
+      -- Only add the left panel on the primary screen
+      if s.index == 1 then
+         left_panel.create(s)
+      end
+
+      -- Add the top panel to every screen
+      top_panel.create(s)
+   end)
+end
+
+return pastel
